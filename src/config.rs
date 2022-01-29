95f00a35a5f1b052a0ee487952770c1bc7ff5015
Julien Kauffmann <julien.kauffmann@freelan.org>
2022-01-29 13:39:59 -0500

    Moved to clap 3

--- src/config.rs
+++ src/config.rs
@@ -1,14 +1,6 @@
 use std::net::SocketAddr;
 
-use clap::{App, Arg, ArgMatches};
-
-const ARG_LISTEN_ENDPOINT: &str = "listen-endpoint";
-const ARG_REVERSE_PROXY_URL: &str = "reverse-proxy-url";
-const ARG_RED_LED_PIN: &str = "red-led-pin";
-const ARG_GREEN_LED_PIN: &str = "green-led-pin";
-const ARG_BUZZER_PIN: &str = "buzzer-pin";
-const ARG_TRIGGER_PIN: &str = "trigger-pin";
-const ARG_ECHO_PIN: &str = "echo-pin";
+use clap::Parser;
 
 const DEFAULT_LISTEN_ENDPOINT: &str = "127.0.0.1:8000";
 const DEFAULT_RED_LED_PIN: &str = "17";
@@ -31,71 +23,69 @@ pub struct GpioConfig {
     pub echo_pin: u8,
 }
 
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(
+        long,
+        short,
+        default_value = DEFAULT_LISTEN_ENDPOINT,
+        value_name = "LISTEN_ENDPOINT"
+    )]
+    pub listen_endpoint: SocketAddr,
+
+    #[clap(long, value_name = "REVERSE_PROXY_URL")]
+    pub reverse_proxy_url: Option<String>,
+
+    #[clap(
+        long,
+        default_value = DEFAULT_RED_LED_PIN,
+        value_name = "RED_LED_PIN"
+    )]
+    pub red_led_pin: u8,
+
+    #[clap(
+        long,
+        default_value = DEFAULT_GREEN_LED_PIN,
+        value_name = "GREEN_LED_PIN"
+    )]
+    pub green_led_pin: u8,
+
+    #[clap(
+        long,
+        default_value = DEFAULT_BUZZER_PIN,
+        value_name = "BUZZER_PIN"
+    )]
+    pub buzzer_pin: u8,
+
+    #[clap(
+        long,
+        default_value = DEFAULT_TRIGGER_PIN,
+        value_name = "TRIGGER_PIN"
+    )]
+    pub trigger_pin: u8,
+
+    #[clap(
+        long,
+        default_value = DEFAULT_ECHO_PIN,
+        value_name = "ECHO_PIN"
+    )]
+    pub echo_pin: u8,
+}
+
 impl Config {
     pub fn new() -> anyhow::Result<Self> {
-        fn simple_arg<'a>(name: &'a str, description: &'a str) -> Arg<'a, 'a> {
-            Arg::with_name(name)
-                .long(name)
-                .help(description)
-                .takes_value(true)
-        }
-
-        fn read_simple_arg<T>(matches: &ArgMatches, name: &str, default: &str) -> anyhow::Result<T>
-        where
-            T: std::str::FromStr,
-            T::Err: Into<anyhow::Error>,
-        {
-            matches
-                .value_of(name)
-                .unwrap_or(default)
-                .parse()
-                .map_err(Into::into)
-        }
-
-        let matches = App::new("home-control")
-            .version(env!("CARGO_PKG_VERSION"))
-            .author(env!("CARGO_PKG_AUTHORS"))
-            .about(env!("CARGO_PKG_DESCRIPTION"))
-            .arg(
-                Arg::with_name(ARG_LISTEN_ENDPOINT)
-                    .long(ARG_LISTEN_ENDPOINT)
-                    .short("l")
-                    .value_name("endpoint")
-                    .help(&format!(
-                        "The endpoint to listen on. Defaults to '{}'",
-                        DEFAULT_LISTEN_ENDPOINT,
-                    ))
-                    .takes_value(true),
-            )
-            .arg(
-                Arg::with_name(ARG_REVERSE_PROXY_URL)
-                    .long(ARG_REVERSE_PROXY_URL)
-                    .short("r")
-                    .value_name("url")
-                    .help("The URL to another server to proxy requests to. Default: does not use a proxy")
-                    .takes_value(true),
-            )
-            .arg(simple_arg(ARG_RED_LED_PIN,  "The GPIO pin to use for the red LED"))
-            .arg(simple_arg(ARG_GREEN_LED_PIN,  "The GPIO pin to use for the green LED"))
-            .arg(simple_arg(ARG_BUZZER_PIN,  "The GPIO pin to use for the buzzer"))
-            .arg(simple_arg(ARG_TRIGGER_PIN,  "The GPIO pin to use for the ultrasonic sensor trigger"))
-            .arg(simple_arg(ARG_ECHO_PIN,  "The GPIO pin to use for the ultrasonic sensor echo"))
-            .get_matches();
+        let args = Args::try_parse()?;
 
         Ok(Self {
-            listen_endpoint: matches
-                .value_of(ARG_LISTEN_ENDPOINT)
-                .unwrap_or(DEFAULT_LISTEN_ENDPOINT)
-                .parse()?,
-            reverse_proxy_url: matches
-                .value_of(ARG_REVERSE_PROXY_URL)
-                .map(|s| s.to_string()),
+            listen_endpoint: args.listen_endpoint,
+            reverse_proxy_url: args.reverse_proxy_url,
             gpio_config: GpioConfig {
-                red_led_pin: read_simple_arg(&matches, ARG_RED_LED_PIN, DEFAULT_RED_LED_PIN)?,
-                green_led_pin: read_simple_arg(&matches, ARG_GREEN_LED_PIN, DEFAULT_GREEN_LED_PIN)?,
-                buzzer_pin: read_simple_arg(&matches, ARG_BUZZER_PIN, DEFAULT_BUZZER_PIN)?,
-                trigger_pin: read_simple_arg(&matches, ARG_TRIGGER_PIN, DEFAULT_TRIGGER_PIN)?,
-                echo_pin: read_simple_arg(&matches, ARG_ECHO_PIN, DEFAULT_ECHO_PIN)?,
+                red_led_pin: args.red_led_pin,
+                green_led_pin: args.green_led_pin,
+                buzzer_pin: args.buzzer_pin,
+                trigger_pin: args.trigger_pin,
+                echo_pin: args.echo_pin,
             },
         })
     }
